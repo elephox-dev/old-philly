7308d2a951758b8e7936d8985f0e2921373d6860
Ricardo Boss <contact@ricardoboss.de>
2020-01-12 22:11:16 +0100

    Added Symfony HttpFoundation

--- src/Philly/App.php
+++ src/Philly/App.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace Philly;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Class App
  */
@@ -15,5 +18,13 @@ class App extends Container\BindingContainer implements Contracts\App
     {
         // bind this app instance to its own class
         $this[App::class] = $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    function handle(Request $request): Response
+    {
+        $this[Request::class] = $request;
     }
 }
--- src/Philly/Contracts/App.php
+++ src/Philly/Contracts/App.php
@@ -3,9 +3,17 @@ declare(strict_types=1);
 
 namespace Philly\Contracts;
 
+use Philly\Contracts\Container\BindingContainer;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Interface App
  */
 interface App extends BindingContainer
 {
+    /**
+     * Handles a request in the app and returns a response.
+     */
+    function handle(Request $request): Response;
 }

b50c717d22884ff66b74186c0544ee4aaa32c906
Ricardo <contact@ricardoboss.de>
2020-01-13 13:41:20 +0100

    Updated Linker

--- src/Philly/Contracts/Routing/LinkerChain.php
+++ src/Philly/Contracts/Routing/LinkerChain.php
@@ -3,11 +3,20 @@ declare(strict_types=1);
 
 namespace Philly\Contracts\Routing;
 
+use Philly\Contracts\App;
 use Philly\Contracts\Container\Container;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Interface LinkerChain
  */
 interface LinkerChain extends Container
 {
+    /**
+     * @param App $app
+     * @param Request $request
+     * @return Response
+     */
+    function pass(App $app, Request $request): Response;
 }
--- src/Philly/Routing/LinkerChain.php
+++ src/Philly/Routing/LinkerChain.php
@@ -3,9 +3,13 @@ declare(strict_types=1);
 
 namespace Philly\Routing;
 
+use Exception;
 use Philly\Container\Container;
+use Philly\Contracts\App;
 use Philly\Contracts\Routing\Linker;
 use Philly\Contracts\Routing\LinkerChain as BaseLinkerChain;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class LinkerChain
@@ -18,5 +22,14 @@ class LinkerChain extends Container implements BaseLinkerChain
     public function accepts($value): bool
     {
         return $value instanceof Linker;
+    }
+
+    /**
+     * @inheritDoc
+     * @throws Exception
+     */
+    function pass(App $app, Request $request): Response
+    {
+
     }
 }

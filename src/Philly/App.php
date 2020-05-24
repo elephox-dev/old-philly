83530c88144480eab0c503ee5532d489deab6e7a
Ricardo Boss <contact@ricardoboss.de>
2020-05-24 06:28:42 +0200

    Removed pipes

--- src/Philly/App.php
+++ src/Philly/App.php
@@ -5,58 +5,41 @@ namespace Philly;
 
 use Philly\Contracts\App as AppContract;
 use Philly\Contracts\Exceptions\ExceptionHandler as ExceptionHandlerContract;
-use Philly\Contracts\Pipeline\Pipeline as PipelineContract;
 use Philly\Exceptions\ExceptionHandler;
-use Philly\Pipeline\Pipeline;
 
 /**
  * Class App
  */
 class App extends Container\BindingContainer implements AppContract
 {
-    /**
-     * @var array
-     */
-    protected array $pipes = [];
+	/**
+	 * @var array
+	 */
+	protected array $pipes = [];
 
-    /**
-     * App constructor.
-     */
-    public function __construct()
-    {
-        parent::__construct();
+	/**
+	 * App constructor.
+	 */
+	public function __construct()
+	{
+		parent::__construct();
 
-        // bind this app instance to its own class
-        $this[AppContract::class] = $this;
-    }
+		// bind this app instance to its own class
+		$this[AppContract::class] = $this;
+	}
 
 	/**
 	 * @inheritDoc
 	 */
-    public function getPipeline(): PipelineContract
-    {
-        if (!$this->has(PipelineContract::class))
-            $this->bind(PipelineContract::class, new Pipeline(), true);
+	public function getExceptionHandler(): ExceptionHandlerContract
+	{
+		if (!$this->has(ExceptionHandlerContract::class))
+			$this->bind(ExceptionHandlerContract::class, new ExceptionHandler(), true);
 
-        $pipeline = $this[PipelineContract::class];
+		$handler = $this[ExceptionHandlerContract::class];
 
-        assert($pipeline instanceof PipelineContract, "Invalid pipeline type!");
+		assert($handler instanceof ExceptionHandlerContract, "Invalid exception handler type!");
 
-        return $pipeline;
-    }
-
-	/**
-	 * @inheritDoc
-	 */
-    public function getExceptionHandler(): ExceptionHandlerContract
-    {
-        if (!$this->has(ExceptionHandlerContract::class))
-            $this->bind(ExceptionHandlerContract::class, new ExceptionHandler(), true);
-
-        $handler = $this[ExceptionHandlerContract::class];
-
-        assert($handler instanceof ExceptionHandlerContract, "Invalid exception handler type!");
-
-        return $handler;
-    }
+		return $handler;
+	}
 }
--- src/Philly/Contracts/App.php
+++ src/Philly/Contracts/App.php
@@ -5,18 +5,12 @@ namespace Philly\Contracts;
 
 use Philly\Contracts\Container\BindingContainer;
 use Philly\Contracts\Exceptions\ExceptionHandler as ExceptionHandlerContract;
-use Philly\Contracts\Pipeline\Pipeline as PipelineContract;
 
 /**
  * Interface App
  */
 interface App extends BindingContainer
 {
-	/**
-	 * @return PipelineContract The pipeline for handling incoming requests.
-	 */
-	public function getPipeline(): PipelineContract;
-
 	/**
 	 * @return ExceptionHandlerContract The handler for exceptions in the application
 	 */

8aeb044173bd1f4594a4a353ecddbfdb2d323c2e
Ricardo Boss <contact@ricardoboss.de>
2020-10-24 00:13:02 +0200

    Filter what can be added to CommandCollection

--- src/CLI/Commands/CommandCollection.php
+++ src/CLI/Commands/CommandCollection.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace Philly\CLI\Commands;
 
 use Philly\Container\Collection;
+use Philly\Contracts\CLI\Commands\Command as CommandContract;
+use Philly\Contracts\CLI\Commands\CommandCollection as CommandCollectionContract;
 
 /**
  * Class CommandCollection.
  */
-class CommandCollection extends Collection implements \Philly\Contracts\CLI\Commands\CommandCollection
+class CommandCollection extends Collection implements CommandCollectionContract
 {
+    public function accepts($value): bool
+    {
+        return $value instanceof CommandContract;
+    }
 }

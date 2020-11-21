82bc95760d068d9e5177432866000ea68875c5cd
Ricardo Boss <contact@ricardoboss.de>
2020-11-21 17:29:03 +0100

    Added alias

--- src/Foundation/CLI/Commands/VersionCommand.php
+++ src/Foundation/CLI/Commands/VersionCommand.php
@@ -8,7 +8,7 @@ use Philly\App;
 use Philly\CLI\Commands\Command;
 use Philly\CLI\Commands\CommandResult;
 use Philly\CLI\Commands\CommandSignature;
-use Philly\Contracts\CLI\Commands\CommandArgumentCollection;
+use Philly\Contracts\CLI\Commands\CommandArgumentCollection as CommandArgumentCollectionContract;
 use Philly\Contracts\CLI\Commands\CommandResult as CommandResultContract;
 
 /**
@@ -21,7 +21,7 @@ class VersionCommand extends Command
         parent::__construct(new CommandSignature("version"));
     }
 
-    public function handle(CommandArgumentCollection $args): CommandResultContract
+    public function handle(CommandArgumentCollectionContract $args): CommandResultContract
     {
         return CommandResult::success(App::VERSION);
     }

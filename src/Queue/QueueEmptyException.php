afd223b77b63a8cfbcdba2a5e25812e77dac6d7a
Ricardo Boss <contact@ricardoboss.de>
2021-01-24 06:44:39 +0100

    Added explicit support for int|mixed scalar type

--- src/Queue/QueueEmptyException.php
+++ src/Queue/QueueEmptyException.php
@@ -5,10 +5,22 @@ declare(strict_types=1);
 namespace Philly\Queue;
 
 use OutOfBoundsException;
+use Throwable;
 
 /**
  * Class QueueEmptyException.
  */
 class QueueEmptyException extends OutOfBoundsException
 {
+    /**
+     * QueueEmptyException constructor.
+     *
+     * @param string $message
+     * @param int|mixed $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
--- src/Stack/StackEmptyException.php
+++ src/Stack/StackEmptyException.php
@@ -5,10 +5,22 @@ declare(strict_types=1);
 namespace Philly\Stack;
 
 use OutOfBoundsException;
+use Throwable;
 
 /**
  * Class StackEmptyException.
  */
 class StackEmptyException extends OutOfBoundsException
 {
+    /**
+     * StackEmptyException constructor.
+     *
+     * @param string $message
+     * @param int|mixed $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }

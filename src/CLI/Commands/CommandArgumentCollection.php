c374d99512a80497dd0fa9e1ffda5fb3d63116d6
Ricardo Boss <contact@ricardoboss.de>
2021-01-25 00:20:26 +0100

    Implemented same logic as in bin/philly

--- src/CLI/Commands/CommandArgumentCollection.php
+++ src/CLI/Commands/CommandArgumentCollection.php
@@ -4,23 +4,30 @@ declare(strict_types=1);
 
 namespace Philly\CLI\Commands;
 
+use InvalidArgumentException;
 use Philly\Contracts\CLI\Commands\CommandArgument as CommandArgumentContract;
 use Philly\Contracts\CLI\Commands\CommandArgumentCollection as CommandArgumentCollectionContract;
+use Philly\Contracts\CLI\Commands\CommandArgumentTemplate as CommandArgumentTemplateContract;
 use Philly\Contracts\CLI\Commands\CommandArgumentTemplateCollection as CommandArgumentTemplateCollectionContract;
 
 class CommandArgumentCollection extends CommandArgumentTemplateCollection implements CommandArgumentCollectionContract
 {
-    public static function fromArray(CommandArgumentTemplateCollectionContract $argumentTemplateCollection, array $args = []): CommandArgumentCollectionContract
-    {
+    public static function fromArray(CommandArgumentTemplateCollectionContract $argumentTemplateCollection, array $args = []): CommandArgumentCollectionContract {
         $collection = new CommandArgumentCollection();
 
-        foreach ($args as $k => $v) {
-            $template = $argumentTemplateCollection->firstKey($k);
-            if ($template == false) {
-                continue;
+        /** @var CommandArgumentTemplateContract $template */
+        foreach ($argumentTemplateCollection as $template) {
+            if (array_key_exists($template->getName(), $args)) {
+                $value = $args[$template->getName()];
+            } elseif (array_key_exists($template->getShortName(), $args)) {
+                $value = $args[$template->getShortName()];
+            } elseif ($template->isOptional()) {
+                $value = $template->getDefaultValue();
+            } else {
+                throw new InvalidArgumentException("Argument {$template->getName()} is required but no value was supplied.");
             }
 
-            $arg = new CommandArgument($template, $v);
+            $arg = new CommandArgument($template, $value);
             $collection->add($arg);
         }
 

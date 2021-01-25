76ccbb7b09e80724cb4292b91f1e4c10079cf420
Ricardo Boss <contact@ricardoboss.de>
2021-01-25 02:01:39 +0100

    Added more tests for different scenarios

--- test/Unit/Foundation/CLI/Commands/CreateCommandCommandTest.php
+++ test/Unit/Foundation/CLI/Commands/CreateCommandCommandTest.php
@@ -6,6 +6,8 @@ namespace test\Philly\Unit\Foundation\CLI\Commands;
 
 use Philly\CLI\Commands\Command;
 use Philly\CLI\Commands\CommandArgumentCollection;
+use Philly\Exceptions\FileNotFoundException;
+use Philly\Exceptions\NullReferenceException;
 use Philly\Foundation\CLI\Commands\CreateCommandCommand;
 use PHPUnit\Framework\TestCase;
 
@@ -68,5 +70,164 @@ class CreateCommandCommandTest extends TestCase
                 unlink($fileLoc);
             }
         }
+    }
+
+    public function testHandleCustomClassname()
+    {
+        $fileLoc = null;
+
+        try {
+            $cmd = new CreateCommandCommand();
+            $tmp = sys_get_temp_dir();
+            $ns = "App1\\test2\\Commands3";
+            /** @noinspection PhpUnhandledExceptionInspection */
+            $rand = random_int(0, 99);
+            $cn = "Other{$rand}Command";
+
+            $r = $cmd->handle(
+                CommandArgumentCollection::fromArray(
+                    $cmd->getSignature()->getArguments(),
+                    [
+                        'name' => "test$rand",
+                        'short' => "t",
+                        'args' => "arg1,arg2,arg3",
+                        'namespace' => $ns,
+                        'dest' => $tmp,
+                        'classname' => $cn
+                    ]
+                )
+            );
+
+            static::assertTrue($r->isSuccess());
+            static::assertNull($r->getThrowable());
+            static::assertIsString($r->getValue());
+
+            /** @var string $fileLoc */
+            $fileLoc = $r->getValue();
+            static::assertTrue(file_exists($fileLoc));
+
+            /** @noinspection PhpIncludeInspection */
+            include $fileLoc;
+
+            $classname = $ns . "\\" . $cn;
+            static::assertTrue(class_exists($classname));
+        } finally {
+            if ($fileLoc !== null && file_exists($fileLoc)) {
+                unlink($fileLoc);
+            }
+        }
+    }
+
+    public function testHandleInvalidStub() {
+        $cmd = new CreateCommandCommand();
+        $tmp = sys_get_temp_dir();
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $rand = random_int(0, 99);
+
+        $r = $cmd->handle(
+            CommandArgumentCollection::fromArray(
+                $cmd->getSignature()->getArguments(),
+                [
+                    'name' => "test$rand",
+                    'dest' => $tmp,
+                    'stub' => "$tmp/I_DON_T_EXIST"
+                ]
+            )
+        );
+
+        static::assertFalse($r->isSuccess());
+        static::assertInstanceOf(FileNotFoundException::class, $r->getThrowable());
+        static::assertNull($r->getValue());
+    }
+
+    public function testHandleNoStub() {
+        $cmd = new CreateCommandCommand();
+        $tmp = sys_get_temp_dir();
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $rand = random_int(0, 99);
+
+        $r = $cmd->handle(
+            CommandArgumentCollection::fromArray(
+                $cmd->getSignature()->getArguments(),
+                [
+                    'name' => "test$rand",
+                    'dest' => $tmp,
+                    'stub' => null
+                ]
+            )
+        );
+
+        static::assertFalse($r->isSuccess());
+        static::assertInstanceOf(NullReferenceException::class, $r->getThrowable());
+        static::assertNull($r->getValue());
+    }
+
+    public function testHandleEmptyArgs() {
+        $fileLoc = null;
+
+        try {
+            $cmd = new CreateCommandCommand();
+            $tmp = sys_get_temp_dir();
+            $ns = "App1\\test2\\Commands3";
+            /** @noinspection PhpUnhandledExceptionInspection */
+            $rand = random_int(0, 99);
+            $cn = "Other{$rand}Command";
+
+            $r = $cmd->handle(
+                CommandArgumentCollection::fromArray(
+                    $cmd->getSignature()->getArguments(),
+                    [
+                        'name' => "test$rand",
+                        'short' => "t",
+                        'args' => ",,,arg1",
+                        'namespace' => $ns,
+                        'dest' => $tmp,
+                        'classname' => $cn
+                    ]
+                )
+            );
+
+            static::assertTrue($r->isSuccess());
+
+            /** @var string $fileLoc */
+            $fileLoc = $r->getValue();
+
+            /** @noinspection PhpIncludeInspection */
+            include $fileLoc;
+
+            $classname = $ns . "\\" . $cn;
+            /** @var Command $c */
+            $c = new $classname();
+            $genSig = $c->getSignature();
+            static::assertCount(1, $genSig->getArguments());
+        } finally {
+            if ($fileLoc !== null && file_exists($fileLoc)) {
+                unlink($fileLoc);
+            }
+        }
+    }
+
+    public function testHandleInvalidDestination()
+    {
+        $cmd = new CreateCommandCommand();
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $rand = random_int(0, 99);
+
+        $r = $cmd->handle(
+            CommandArgumentCollection::fromArray(
+                $cmd->getSignature()->getArguments(),
+                [
+                    'name' => "test$rand",
+                    'dest' => "NO_DRIVE/",
+                ]
+            )
+        );
+
+        static::assertFalse($r->isSuccess());
+        static::assertInstanceOf(FileNotFoundException::class, $r->getThrowable());
+        static::assertNull($r->getValue());
     }
 }

f54ce4e00e4aa582a4e9e1a53f894a60812e454f
Ricardo Boss <contact@ricardoboss.de>
2021-01-25 23:22:12 +0100

    Added isBooted method and validation when booting is happening

--- src/ServiceProvider/ServiceProviderContainer.php
+++ src/ServiceProvider/ServiceProviderContainer.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace Philly\ServiceProvider;
 
 use Philly\Container\BindingContainer;
-use Philly\Contracts\ServiceProvider\ServiceProviderContainer as ServiceProviderContainerContract;
 use Philly\Contracts\ServiceProvider\ServiceProvider as ServiceProviderContract;
+use Philly\Contracts\ServiceProvider\ServiceProviderContainer as ServiceProviderContainerContract;
 
 /**
  * Class ServiceProviderContainer.
  */
 class ServiceProviderContainer extends BindingContainer implements ServiceProviderContainerContract
 {
+    protected bool $booted = false;
+    protected bool $booting = false;
+
     /**
      * @inheritDoc
      */
@@ -29,9 +32,22 @@ class ServiceProviderContainer extends BindingContainer implements ServiceProvid
      */
     public function offsetSet($offset, $value)
     {
+        if ($this->booted) {
+            throw new AlreadyBootedException("Service provider container was already booted!");
+        }
+
         parent::offsetSet($offset, $value);
 
         $value->onRegistered();
+    }
+
+    public function offsetGet($offset)
+    {
+        if (!$this->booted && !$this->booting) {
+            $this->boot();
+        }
+
+        return parent::offsetGet($offset);
     }
 
     /**
@@ -39,9 +55,22 @@ class ServiceProviderContainer extends BindingContainer implements ServiceProvid
      */
     public function boot(): void
     {
+        if ($this->booted || $this->booting)
+            throw new AlreadyBootedException("Service provider container ".__CLASS__." was already booted.");
+
+        $this->booting = true;
+
         /** @var ServiceProviderContract $service */
         foreach ($this as $service) {
             $service->onBooted();
         }
+
+        $this->booting = false;
+        $this->booted = true;
+    }
+
+    public function isBooted(): bool
+    {
+        return $this->booted;
     }
 }
